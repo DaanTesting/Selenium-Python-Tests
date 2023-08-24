8efd6d828ac183363b47c9de6ae697745f56eb73
Samuel <samuel.beurms@optimile.eu>
2023-08-24 09:44:26 +0200

    [settings] Add Settings.example.py and add Settings.py to gitignore.

---
+++ utilities/Settings.example.py
@@ -0,0 +1,5 @@
+# Copy this files into Settings.py and update the directories to the correct locations.
+
+cache_directory = "/home/samuel/Downloads/SeleniumCache"
+chromedriver_directory = "/home/samuel/Documents/chromedriver"
+screenshot_directory = "/home/samuel/Documents/Screenshots/{}"
--- utilities/Settings.py
+++
@@ -1,3 +0,0 @@
-cache_directory = "/Users/daanswinnen/Downloads/SeleniumCache"
-chromedriver_directory = "/Users/daanswinnen/Documents/chromedriver"
-screenshot_directory = "/Users/daanswinnen/Documents/Screenshots/{}"
